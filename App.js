6f2bc7c93222e83c77d477fe8b7f65d170f7372d
songoku311 <duythanh3101@gmail.com>
2020-03-14 15:12:56 +0700

    Initialize project

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+import { AppLoading } from 'expo'; 
+import * as Font from 'expo-font';
+
+const getFonts = () => {
+    return Font.loadAsync({
+        'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+        'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf')
+      });
+  };
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (fontLoaded){
+    return (
+      <Home/>
+    );
+  }
+  else{
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish= {() => setFontLoaded(true)}
+      />
+    );
+  }
+ 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    titleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+        color: '#333'
+    },
+    paragraph: {
+        lineHeight: 20,
+        marginVertical: 8,
+    }
+});
