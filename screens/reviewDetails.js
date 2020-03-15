23ab11ca282a7913648c256c07714c33b3438923
songoku311 <duythanh3101@gmail.com>
2020-03-15 14:02:09 +0700

    Using image to display rating in card component

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }){
@@ -8,16 +8,31 @@ export default function ReviewDetails({ navigation }){
     const pressHandler = () => {
         navigation.goBack();
     }
+    
+    const rating = navigation.getParam('rating');
 
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{navigation.getParam('title')}</Text>
                 <Text>{navigation.getParam('body')}</Text>
-                <Text>{navigation.getParam('rating')}</Text>
+                <View style={styles.rating}>
+                    <Text>Gamezone rating: </Text>
+                    <Image source={images.rating[rating]}/>
+                </View>
             </Card>
             
         </View>
     );
     
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        borderTopColor: 'red',
+        borderTopWidth: 1,
+        justifyContent: 'center',
+        marginTop: 10
+    }
+})
